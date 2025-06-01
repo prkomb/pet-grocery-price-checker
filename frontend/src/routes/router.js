bd16d16ebbcac346fbd14ae068caf5ed11388b6f
Khahani Mamedov <prkomb@gmail.com>
2025-06-01 23:40:59 +0100

    started to develop new page

--- frontend/src/routes/router.js
+++ frontend/src/routes/router.js
@@ -2,8 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
 import NotFound from "../pages/NotFound";
-import ResponsiveHeader from "../components/Home/Header/ResponsiveHeader/ResponsiveHeader";
-import ResponseTestimonals from "../components/Home/Testimonials/ResponsiveTestimonals/ResponsiveTestimonals";
 import WelcomePage from "../pages/WelcomePage";
 import AuthForm from "../layouts/AuthForm";
 import HeaderLayout from "../layouts/Header";
@@ -11,7 +9,8 @@ import Profile from "../pages/Profile";
 import AboutUs from "../pages/AboutUs";
 import Categories from "../pages/Categories";
 import CurrentCategory from "../pages/CurrentCategory";
-import FakeReload from "../layouts/FakeReload";
+import ProductPage from "../pages/ProductPage";
+
 export default createBrowserRouter([
   {
     path: "/",
@@ -25,6 +24,7 @@ export default createBrowserRouter([
       { path: "home", Component: Home },
       { path: "products", Component: Products },
       { path: "profile", Component: Profile },
+      { path: "/product/:productId", Component: ProductPage },
     ],
   },
 
